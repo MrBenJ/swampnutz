b88972854458fd9afa7ea871d80ad1e8a0b80131
Ben Junya <bjunya@gmail.com>
2019-07-05 17:42:02 -0700

    First pass of script

---
+++ README.md
@@ -0,0 +1,4 @@
+Quicky md site
+==============
+Genearate a quick markdown static site super fast with these handy scripts and command prompts
+
---
+++ index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const Minimist = require('minimist');
+const nunjucks = require('nunjucks');
+
+const { path: filePath } = Minimist(process.argv.slice(2));
+
+
+const render = promisify(nunjucks.render);
+function getFilename(filepath) {
+  const [ ext, file ] = filepath.split('.').reverse();
+
+  const [ name ] = file.split('/').reverse();
+
+  return name;
+}
+
+function init() {
+  // TODO: Change this to the name of the file
+  const BUILD_PATH = path.resolve(process.cwd(), 'dist');
+  const content = fs.readFileSync(
+    path.resolve(process.cwd(), filePath)
+  ).toString();
+
+  render(path.resolve(__dirname, './template.njk'), { md: content }).then( file => {
+
+    if (!fs.existsSync()) {
+      fs.mkdirSync(BUILD_PATH);
+    }
+
+    fs.writeFileSync(path.resolve(BUILD_PATH, './index.html'), file);
+  });
+}
+
+init();
